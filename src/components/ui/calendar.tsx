2969fe0a892a9229d7878196bda6e774f63c17af
mohsink54 <mohsin.jsdeveloper@gmail.com>
2025-05-06 14:31:00 +0500

    calender fix

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -2,19 +2,30 @@
 
 import * as React from "react"
 import DatePicker from "react-datepicker" 
-
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
 import "react-datepicker/dist/react-datepicker.css" // import the css for react-datepicker
 
+type CalendarProps = React.ComponentProps<typeof DatePicker>;
+
 function Calendar({
   className,
+  onChange,
   ...props
-}: React.ComponentProps<typeof DatePicker>) {
+}: CalendarProps) {
+  const handleChange = (date: Date | [Date | null, Date | null] | null, event?: React.SyntheticEvent) => {
+    // If the date is an array (range selection), pass the appropriate value to the onChange
+    if (Array.isArray(date)) {
+      onChange?.(date as [Date | null, Date | null], event); // Handle range date selection
+    } else {
+      onChange?.(date as Date | null, event); // Handle single date selection
+    }
+  }
+
   return (
     <DatePicker
       selected={props.selected}
-      onChange={props.onChange}
+      onChange={handleChange}
       inline
       className={cn("p-3", className)}
       popperPlacement="bottom"
